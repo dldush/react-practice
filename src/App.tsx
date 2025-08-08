8b899ccf65da7ee888546fee9648dec401d4be9b
이연호 <leecarus0301@naver.com>
2025-08-08 13:41:42 +0900

    [INIT] init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hooks</h1>
+        <ol>
+          <li>useState</li>
+          <li>useEffect</li>
+          <li>useContext</li>
+          <li>useReducer</li>
+          <li>useCallback</li>
+          <li>useMemo</li>
+        </ol>
+      </div>
+    </>
+  );
+}
+
+export default App;
